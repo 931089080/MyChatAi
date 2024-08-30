64a322694e6b0fd414d1ff85f85dd8fb992cdb1f
owo <931089080@qq.com>
2024-08-30 16:38:32 +0800

    优化sse

--- src/main/java/com/example/utils/SseSendMessageThread.java
+++ src/main/java/com/example/utils/SseSendMessageThread.java
@@ -6,11 +6,9 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.example.client.SseClient;
 import com.example.manager.SseManager;
-import com.example.model.RoleContent;
 import com.example.model.dto.request.MessageVO;
 import com.example.model.vo.ClientToMessageVo;
 import com.example.properties.SparkAiProperties;
-import com.google.gson.Gson;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.MediaType;
@@ -23,7 +21,6 @@ import okhttp3.sse.EventSources;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,41 +43,37 @@ public class SseSendMessageThread extends Thread {
 
     @Override
     public void run() {
-
-        List<MessageVO> messageVOS = SseManager.historyList.getOrDefault(clientToMessageVo.getClientId(), new ArrayList<>());
-        if (messageVOS.isEmpty()) {
-            SseManager.historyList.put(clientToMessageVo.getClientId(), messageVOS);
+        //获取历史聊天记录
+        List<MessageVO> historyList = SseManager.historyList.getOrDefault(clientToMessageVo.getClientId(), new ArrayList<>());
+        if (historyList.isEmpty()) {
+            //为空 新会话 首次发送请求
+            SseManager.historyList.put(clientToMessageVo.getClientId(), historyList);
         }
 
-        JSONObject requestJson = new JSONObject();
-
-        MessageVO messageVO = new MessageVO("user", clientToMessageVo.getData());
-
-        requestJson.put("model", sparkAiProperties.getDomain());
-
         JSONArray messages = new JSONArray();
-        //获取历史消息
-        List<MessageVO> historyList = SseManager.historyList.get(clientToMessageVo.getClientId());
+        //有历史消息 放入请求体中
         if (historyList.size() > 0) {
             for (MessageVO list : historyList) {
                 messages.add(list);
             }
         }
+        MessageVO messageVO = new MessageVO("user", clientToMessageVo.getData());
         messages.add(messageVO);
 
+        JSONObject requestJson = new JSONObject();
+        requestJson.put("model", sparkAiProperties.getDomain());
         requestJson.put("messages", messages);
         requestJson.put("stream", true);
         log.info("请求body -> {}", requestJson);
 
-        OkHttpClient client = new OkHttpClient();
-
-        EventSource.Factory factory = EventSources.createFactory(client);
+        //构建请求
         Request request = new Request.Builder()
                 .url(sparkAiProperties.getSseUrl())
-                .post(okhttp3.RequestBody.create(requestJson.toString(), MediaType.parse("application/json; charset=utf-8")))
+                .post(okhttp3.RequestBody.create(MediaType.parse("application/json; charset=utf-8"), requestJson.toString()))
                 .addHeader("Authorization", "Bearer " + sparkAiProperties.getAPIPassword())
                 .build();
 
+        //设置sse监听回调
         EventSourceListener listener = new EventSourceListener() {
             @Override
             public void onEvent(@NotNull EventSource eventSource, @Nullable String id, @Nullable String type, @NotNull String data) {
@@ -96,11 +89,11 @@ public class SseSendMessageThread extends Thread {
                     JSONObject choice = choices.getJSONObject(0);
                     JSONObject delta = choice.getJSONObject("delta");
                     String content = delta.getString("content");
+                    //打印接收ai 解析的消息
                     System.out.print(content);
                     sseClient.sendMessageToOneClient(clientToMessageVo.getClientId(), content);
                     totalAnswer += content;
                 }
-
             }
 
             @Override
@@ -127,27 +120,30 @@ public class SseSendMessageThread extends Thread {
             }
         };
 
+        OkHttpClient client = new OkHttpClient();
+        EventSource.Factory factory = EventSources.createFactory(client);
         // 创建事件
         EventSource eventSource = factory.newEventSource(request, listener);
 
-        canAddHistory(messageVOS);
-        //存入发送历史消息
-        messageVOS.add(messageVO);
-        log.info("sse历史消息 -> {}", SseManager.historyList.get(clientToMessageVo.getClientId()));
+        canAddHistory(historyList, messageVO);
+//        log.info("sse历史消息 -> {}", SseManager.historyList.get(clientToMessageVo.getClientId()));
     }
 
-    // 历史记录最大上线1.2W左右
-    public static void canAddHistory(List<MessageVO> historyList) {
+    //历史记录最大上线1.2W左右
+    public static void canAddHistory(List<MessageVO> historyList, MessageVO messageVO) {
         int history_length = 0;
         for (MessageVO temp : historyList) {
             history_length = history_length + temp.getContent().length();
         }
-        log.info("当前历史记录长度 -> {}", history_length);
+//        log.info("当前历史记录长度 -> {}", history_length);
+        // 删除前面记录会把相应的内存空间也删除 并且下标前移
         if (history_length > 12000) {
             for (int i = 0; i < 5; i++) {
                 historyList.remove(i);
             }
         }
+        //存入发送历史消息
+        historyList.add(messageVO);
     }
 
 }
